d4a07225ecc6fbce090bc1380a0e922e8e38a9ab
Matheus M. Viana <matheusmviana@outlook.com.br>
2025-04-02 21:23:22 -0300

    Refactor: Changing user table pk to "user_id"

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,38 +1,45 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY || "development";
 const user = require("../controllers/UserController");
 require("dotenv").config();
 
 function authMiddleware(roles = []) {
   return async (req, res, next) => {
-    const token = req.headers["authorization"];
-
-    if (!token) {
-      return res
-        .status(401)
-        .send({ error: "Access denied. No token provided." });
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(401).send({ error: "Access denied. No token provided." });
     }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).send({ error: "Token not provided." });
+    }
+
     try {
-      const decoded = jwt.verify(token, SECRET_KEY);
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+      console.log("Decoded token:", decoded);
+
+      if (!decoded) {
+        return res.status(401).send({ error: "Decoded was not found." });
+      }
 
       const userLogged = await user.findUser(decoded.id);
       if (!userLogged) {
-        return res.status(500).json("User not found.");
+        return res.status(404).json("User not found.");
       }
 
       if (roles.length && !roles.includes(userLogged.role)) {
-        return res.status(500).json("User without permission.");
+        return res.status(403).json("User without permission.");
       }
-
-      req.user = decoded;
 
       if (userLogged.role === "blocked") {
-        return res.status(500).json("User blocked.");
+        return res.status(403).json("User blocked.");
       }
 
+      req.user = userLogged;
       next();
     } catch (err) {
-      return res.status(401).send({ error: "Invalid token." });
+      return res.status(401).send({ error: "Invalid token.", details: err.message });
     }
   };
 }
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,43 +1,32 @@
+const { DataTypes } = require("sequelize");
 const database = require("../config/Database");
 
-class UserModel {
-  constructor() {
-    this.model = database.db.define("users", {
-      id: {
-        type: database.db.Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: database.db.Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: database.db.Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true, // Valida se o email tem o formato correto
-        },
-      },
-      password: {
-        type: database.db.Sequelize.STRING,
-        allowNull: false,
-      },
-      role: {
-        type: database.db.Sequelize.ENUM("admin", "viewer", "blocked", "salesperson"),
-        allowNull: false,
-      },
-      // accessCode: {
-      //   type: database.db.Sequelize.STRING,
-      //   allowNull: true,
-      // },
-      // accessCodeExpiration: {
-      //   type: database.db.Sequelize.DATE,
-      //   allowNull: true,
-      // },
-    });
-  }
-}
+const UserModel = database.define("User", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM("salesperson", "admin", "viewer", "blocked"),
+    allowNull: false,
+  },
+});
 
-module.exports = new UserModel().model;
+module.exports = UserModel;
