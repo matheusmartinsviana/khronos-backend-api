075a1cdb0cdf00c7df3fca6350c7b56473b171cb
Matheus Viana <matheusmviana@outlook.com.br>
2025-06-05 00:14:03 -0300

    feat: Refactor SaleRoute to integrate SaleController and remove unused routes

--- src/routes/SaleRoute.js
+++ src/routes/SaleRoute.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/sheetProductDataController");
-const authMiddleware = require("../middlewares/auth");
+const SaleController = require("../controllers/SaleController");
 
-router.get("/getData", authMiddleware(), controller.getData);
-router.get("/getRows", authMiddleware(), controller.getRows);
-router.post("/addRow", authMiddleware(), controller.addRow);
-router.post("/updateValue", authMiddleware(), controller.updateValue);
-router.post("/import-products", authMiddleware(), controller.importProductController);
+router.post("/", SaleController.create);
+router.get("/", SaleController.getAll);
+router.get("/:id", SaleController.getById);
+router.put("/:id", SaleController.update);
+router.delete("/:id", SaleController.delete);
 
 module.exports = router;
